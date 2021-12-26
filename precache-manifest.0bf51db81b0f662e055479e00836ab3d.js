2a6645f5cf384d517822e96eb56dd4fc3fe6995a
Oystein Eftevaag <oystein@geheb.com>
2021-12-26 15:23:54 -0800

    Initial build

---
+++ precache-manifest.0bf51db81b0f662e055479e00836ab3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c5c61ddf9f262528b8675a39f4b77a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19ff9f0f0b6c23c3802d",
+    "url": "./static/css/2.e590f925.chunk.css"
+  },
+  {
+    "revision": "f6c062c4c4e92858948e",
+    "url": "./static/css/main.9016c938.chunk.css"
+  },
+  {
+    "revision": "19ff9f0f0b6c23c3802d",
+    "url": "./static/js/2.53de0d57.chunk.js"
+  },
+  {
+    "revision": "05e69800b83aa89a99e57af734343c9b",
+    "url": "./static/js/2.53de0d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c062c4c4e92858948e",
+    "url": "./static/js/main.a9ade7a7.chunk.js"
+  },
+  {
+    "revision": "da503e7975902b161ea8",
+    "url": "./static/js/runtime-main.7d63d1b5.js"
+  },
+  {
+    "revision": "a4e80c1e977416ec45d808a3268892b7",
+    "url": "./static/media/builton.a4e80c1e.svg"
+  }
+]);
